689b2135c994990ec2510d366ffe18d81ea686b9
jreleaserbot <jreleaser@kordamp.org>
2023-09-28 09:01:58 +0100

    spring-cli v0.7.1

--- Formula/spring-cli.rb
+++ Formula/spring-cli.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.8.0 at 2023-09-27T10:52:20.254064498+01:00
+# Generated with JReleaser 1.8.0 at 2023-09-28T08:58:37.062145827+01:00
 class SpringCli < Formula
   desc "Spring Cli"
   homepage "https://github.com/spring-projects-experimental/spring-cli"
   url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.1/spring-cli-0.7.1.zip"
   version "0.7.1"
-  sha256 "c9b0b1ba8c5e2ed445f51c509d8e233b505608f1c45714d821cc3d8f06e7c183"
+  sha256 "a63bfb384f56a76403bcba89f20bee95c18e06f8ac26a867901ce51025176ecd"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
