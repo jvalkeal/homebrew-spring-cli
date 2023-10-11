be709b3dec76dd4f47ac4741864a3dd21ffc7d8e
jreleaserbot <jreleaser@kordamp.org>
2023-10-11 17:03:44 +0000

    spring-cli-standalone v0.7.1

--- Formula/spring-cli.rb
+++ Formula/spring-cli.rb
@@ -1,21 +1,34 @@
-# Generated with JReleaser 1.8.0 at 2023-09-28T08:58:37.062145827+01:00
+# Generated with JReleaser 1.9.0-SNAPSHOT at 2023-10-11T17:03:16.966490885Z
 class SpringCli < Formula
   desc "Spring Cli"
   homepage "https://github.com/spring-projects-experimental/spring-cli"
-  url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.1/spring-cli-0.7.1.zip"
   version "0.7.1"
-  sha256 "a63bfb384f56a76403bcba89f20bee95c18e06f8ac26a867901ce51025176ecd"
   license "Apache-2.0"
 
-  depends_on "openjdk@17"
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.1/spring-cli-standalone-0.7.1-linux.x86_64.zip"
+    sha256 "7fd4eca67c1b3148abb1277e4a2ee8aa6e6c779d915e991738225d99ae6c9d9e"
+  end
+
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/spring" => "spring"
+    bin.install_symlink "#{libexec}/bin/spring-cli-standalone" => "spring-cli-standalone"
+  end
+
+  def post_install
+    if OS.mac?
+      Dir["#{libexec}/lib/**/*.dylib"].each do |dylib|
+        chmod 0664, dylib
+        MachO::Tools.change_dylib_id(dylib, "@rpath/#{File.basename(dylib)}")
+        MachO.codesign!(dylib) if Hardware::CPU.arm?
+        chmod 0444, dylib
+      end
+    end
   end
 
   test do
-    output = shell_output("#{bin}/spring --version")
+    output = shell_output("#{bin}/spring-cli-standalone --version")
     assert_match "0.7.1", output
   end
 end
