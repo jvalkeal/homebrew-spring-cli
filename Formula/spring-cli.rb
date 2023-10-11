c5b3cf2f077056284cbfbd453616d22c37951893
jreleaserbot <jreleaser@kordamp.org>
2023-10-11 18:43:14 +0000

    spring-cli-standalone v0.7.1

--- Formula/spring-cli.rb
+++ Formula/spring-cli.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.9.0-SNAPSHOT at 2023-10-11T17:03:16.966490885Z
+# Generated with JReleaser 1.9.0-SNAPSHOT at 2023-10-11T18:42:37.35253352Z
 class SpringCli < Formula
   desc "Spring Cli"
   homepage "https://github.com/spring-projects-experimental/spring-cli"
@@ -7,13 +7,21 @@ class SpringCli < Formula
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.1/spring-cli-standalone-0.7.1-linux.x86_64.zip"
-    sha256 "7fd4eca67c1b3148abb1277e4a2ee8aa6e6c779d915e991738225d99ae6c9d9e"
+    sha256 "3a5c0e3f92c32e275fc7919c56b4801bcb02b4b09ac7d1cd9bf76208263cd560"
+  end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.1/org.springframework.experimental.spring-cli-0.7.1-osx-x86_64.zip"
+    sha256 "fa5f584a42899f4e00baaf8975882b3c10457b2e02c0bda1d970d74ee40c7581"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.1/org.springframework.experimental.spring-cli-0.7.1-osx-x86_64.zip"
+    sha256 "fa5f584a42899f4e00baaf8975882b3c10457b2e02c0bda1d970d74ee40c7581"
   end
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/spring-cli-standalone" => "spring-cli-standalone"
+    bin.install_symlink "#{libexec}/bin/spring" => "spring"
   end
 
   def post_install
@@ -28,7 +36,7 @@ class SpringCli < Formula
   end
 
   test do
-    output = shell_output("#{bin}/spring-cli-standalone --version")
+    output = shell_output("#{bin}/spring --version")
     assert_match "0.7.1", output
   end
 end
