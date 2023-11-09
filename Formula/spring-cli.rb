4b75d039b3c469809bcbcf09bedcd6b7863eadd6
jreleaserbot <jreleaser@kordamp.org>
2023-11-09 12:52:00 +0000

    spring-cli-standalone v0.7.6

--- Formula/spring-cli.rb
+++ Formula/spring-cli.rb
@@ -1,27 +1,28 @@
-# Generated with JReleaser 1.10.0-SNAPSHOT at 2023-11-06T11:07:04.568909648Z
+# Generated with JReleaser 1.10.0-SNAPSHOT at 2023-11-09T12:51:07.841858447Z
 class SpringCli < Formula
   desc "Spring Cli"
   homepage "https://github.com/spring-projects-experimental/spring-cli"
-  version "0.7.5"
+  version "0.7.6"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.5/spring-cli-standalone-0.7.5-linux.x86_64.zip"
-    sha256 "c803ff455f4b214ebdd68b95f21f8bd2845caf86cbd3358ca9f74a37401d916a"
+    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.6/spring-cli-standalone-0.7.6-linux.x86_64.zip"
+    sha256 "c53c39928ea1035e622613df15ad046cb0db306145de91b16420209ae64beb93"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.5/spring-cli-standalone-0.7.5-osx.aarch64.zip"
-    sha256 "fa9d6fc7399c6236200b095fe7576dbec366c1901bd8136865770d4e7a27ff3f"
+    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.6/spring-cli-standalone-0.7.6-osx.aarch64.zip"
+    sha256 "0ef11d855b1d6b22c19b8ed97accaef1910c6c96d474286392771c7222f0f9a8"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.5/spring-cli-standalone-0.7.5-osx.x86_64.zip"
-    sha256 "d8353c007f2759966b116c4bf4efaee4d02222215d2dec64045f3e9e6186d53c"
+    url "https://github.com/jvalkeal/spring-cli/releases/download/v0.7.6/spring-cli-standalone-0.7.6-osx.x86_64.zip"
+    sha256 "5da56ebcd2d1f6a07698766932da19d9540dfb6f74372ceb2c7ee6e24361ea31"
   end
 
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/spring" => "spring"
+    bash_completion.install Dir["#{libexec}/completion/bash/spring"]
   end
 
   def post_install
@@ -37,6 +38,6 @@ class SpringCli < Formula
 
   test do
     output = shell_output("#{bin}/spring --version")
-    assert_match "0.7.5", output
+    assert_match "0.7.6", output
   end
 end
